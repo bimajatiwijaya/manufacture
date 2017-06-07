a5b8e65aea51a2f1f8173f48c91000a672cc4de1
bima <bimajatiwijaya@gmail.com>
2017-06-07 18:44:04 +0700

    first dev.. mrp.property not exist on odoo 10.0

--- mrp_production_properties/__manifest__.py
+++ mrp_production_properties/__manifest__.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Alex Comba <alex.comba@agilebg.com>
-#    Copyright (C) 2014 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 <alex.comba@agilebg.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Mrp Production Properties",
     'version': '0.1',
@@ -39,5 +21,5 @@ from the related procurement order.
     'test': [
         'test/mrp_production_properties.yml',
     ],
-    'installable': False
+    'installable': True,
 }
--- mrp_production_properties/mrp.py
+++ mrp_production_properties/mrp.py
@@ -1,58 +1,32 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Alex Comba <alex.comba@agilebg.com>
-#    Copyright (C) 2014 Agile Business Group sagl
-#    (<http://www.agilebg.com>)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-from openerp.osv import orm, fields
+# Copyright 2014 <alex.comba@agilebg.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models, fields
 
 
-class mrp_production(orm.Model):
+class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    _columns = {
-        'property_ids': fields.many2many(
-            'mrp.property',
-            'mrp_production_property_rel',
-            'production_id',
-            'property_id',
-            'Properties'
-        ),
-    }
+    property_ids = fields.Many2many('mrp.property',
+                                    'mrp_production_property_rel',
+                                    'production_id', 'property_id',
+                                    string='Properties')
 
 
-class procurement_order(orm.Model):
+class ProcurementOrder(models.Model):
     _inherit = "procurement.order"
 
-    def make_mo(self, cr, uid, ids, context=None):
-        res = super(procurement_order, self).make_mo(
-            cr, uid, ids, context=context)
-        production_obj = self.pool.get('mrp.production')
+    @api.multi
+    def make_mo(self):
+        res = super(ProcurementOrder, self).make_mo()
+        production_obj = self.env['mrp.production']
         for procurement_id, produce_id in res.iteritems():
-            procurement = self.browse(
-                cr, uid, procurement_id, context=context)
-            production = production_obj.browse(
-                cr, uid, produce_id, context=context)
+            procurement = self.browse(procurement_id)
+            production = production_obj.browse(produce_id)
             vals = {
                 'property_ids': [
                     (6, 0, [x.id for x in procurement.property_ids])
                 ]
             }
-            production.write(vals, context=context)
+            production.write(vals)
         return res
